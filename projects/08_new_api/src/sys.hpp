e71715467563ea44a3d51069707669ac004d5902
aod6060 <a.o.d.6060@gmail.com>
2025-10-31 08:22:44 -0500

    Adding 08_new_api this will change up how I create demos.

---
+++ projects/08_new_api/src/sys.hpp
@@ -0,0 +1,20 @@
+#ifndef SYS_HPP
+#define SYS_HPP
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <SDL3/SDL.h>
+#include <SDL3_image/SDL_image.h>
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_sdl3.h"
+#include "imgui/imgui_impl_opengl3.h"
+
+#endif
